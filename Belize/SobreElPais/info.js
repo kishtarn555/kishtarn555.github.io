6ce2e556e6a8f01752cb0830cc7a0e2a68dc595c
David Paredes <0238526@up.edu.mx>
2023-03-29 15:48:13 -0600

    belice

---
+++ Belize/SobreElPais/info.js
@@ -0,0 +1,128 @@
+const historyData1 =  [
+    {
+        img: "ingl.png", //la otra parte de la ruta esta en cards
+        title:"Inglés",
+        text:"Único país de centroamérica con idioma oficial. El 52% habla también Español. 44.6% Habla criollo beliceño. Fue colonia británica: Honduras Británica."
+    },
+    {
+        img: "moned.jpeg",
+        title:"Moneda",
+        text:"Dólar beliceño (1USD  = 2.02 BZD). Tiene el rostro del Rey del Reino Unido."
+    },
+    {
+        img: "simbo.jpg",
+        title:"Simbolos nacionales",
+        text:"Flor: orquídea negra; Árbol de caoba. Sub Umbra Floreo; Tapir, ave: tucán pico Canoa."
+    },
+
+]
+
+const historyData2 =  [
+    {
+        img: "sign.jpeg", //la otra parte de la ruta esta en cards
+        title:"Significado",
+        text:"Maya Balis: Fangoso. Río Belice."
+    },
+    {
+        img: "capi.jpeg",
+        title:"Capital",
+        text:"Belmopán. una de las más nuevas del mundo. Luego de que un huracán destruyó la anterior en 1981."
+    },
+    {
+        img: "indepe.jpg",
+        title:"Independencia",
+        text:"Último de América. 1981. Rey de Reino Unido es soberano."
+    },
+
+]
+
+const pobData = [
+    {
+        img: "pobla.jpg", //la otra parte de la ruta esta en cards
+        title:"De los menos poblados del mundo.",
+        text:"177/200"
+    },
+    {
+        img: "gari.jpg",
+        title:"Garífunas.",
+        text:"Grupo étnico afrodescendiente. Su música y cultura son Patrimonio de la Humanidad."
+    },
+    {
+        img: "biodi.jpg",
+        title:"Gran biodiversidad. ",
+        text:"Clima subtropical. 3408 especies de plantas, 46 de anfibios, 141 de reptiles, 163 de mamiferos, 1302 especies de peces. "
+    },
+
+]
+
+const ecoData = [
+    {
+        img: "turi.png", //la otra parte de la ruta esta en cards
+        title:"Turismo ecológico.",
+        text:"El sector terciario representa el 72% del PIB."
+    },
+    {
+        img: "azu.jpg",
+        title:"Gran agujero azul.",
+        text:"Experiencia de buceo. Popular. (32 m)"
+    },
+    {
+        img: "peque.jpg",
+        title:"Economía más pequeña de América. Debido a su población. 170/200.",
+        text:" Debido a su población. 170/200."
+    },
+    {
+        img: "corru.jpg",
+        title:"Corrupción.",
+        text:"Pensamiento común."
+    }
+
+]
+
+const cultData = [
+    {
+        img: "gastro.jpeg", //la otra parte de la ruta esta en cards
+        title:"Gastronomía",
+        text:"Mezcla de comida británica, estadounidense, mexicana y caribeña."
+    },
+    {
+        img: "depo.jpeg",
+        title:"Deporte",
+        text:"Poco remunerado, no tiene ningún título internacional."
+    },
+    {
+        img: "maya.jpg",
+        title:"Es parte del Imperio Maya.",
+        text:" Región del caracol. "
+    },
+    {
+        img: "patri.jpg",
+        title:"Barrera de Coral.",
+        text:"Patrimonio de la humanidad. Conservación de especies en peligro."
+    }
+
+]
+
+const intData = [
+    {
+        img: "selv.jpg", //la otra parte de la ruta esta en cards
+        title:"Selvas",
+        text:"Reserva natural de especies de países vecinos."
+    },
+    {
+        img: "band.png",
+        title:"Bandera única. ",
+        text:"Con humanos (junto a Malta)"
+    },
+    {
+        img: "arrec.png",
+        title:"Arrecife mesoamericano. ",
+        text:"Segundo arrecife de coral más grande del mundo. 1000km."
+    },
+    {
+        img: "jagu.jpg",
+        title:"Santuario de conservación de jaguares.",
+        text:"Único país del mundo."
+    }
+
+]
