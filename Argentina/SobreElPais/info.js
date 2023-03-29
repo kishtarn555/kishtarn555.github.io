7f4545dbc23d6aa5661e0191ed0b80889b1757c1
David Paredes <0238526@up.edu.mx>
2023-03-29 07:27:17 -0600

    cambios-argentina

--- Argentina/SobreElPais/info.js
+++ Argentina/SobreElPais/info.js
@@ -35,3 +35,41 @@ const historyData2 =  [
     },
 
 ]
+
+const paisajes_data_1 =  [
+    {
+        img: "SALINAS.jpg",
+        title:"Las Salinas Grandes",
+        text:"El tercer salar mas grande de Sudamérica. 12,000 hectáreas."
+    },
+    {
+        img: "Glaciar.jpg",
+        title:"El Glaciar Perito Moreno",
+        text:"Se extiende desde la cordillera de los Andes hasta el lago Argentino"
+    },
+    {
+        img: "Talampaya.jpg",
+        title:"Parque Nacional Talampaya",
+        text:"Una formación rocosa de imponentes paredes verticales"
+    },
+
+]
+
+const paisajes_data_2 =  [
+    {
+        img: "Misionera.jpg",
+        title:"La Selva Misionera",
+        text:"Una de las eco-regiones más diversas del planeta.Hogar del yaguareté y del tatú-carreta, entre otros animales y más de 400 especies de aves. "
+    },
+    {
+        img: "Clavero.jpg",
+        title:"El río Mina Clavero",
+        text:"Sus aguas claras y mineralizadas nacen en la Pampa de Achala."
+    },
+    {
+        img: "LAESTRELLA.jpg",
+        title:"Bañado La Estrella",
+        text:"Ubicado al norte de la localidad de Las Lomitas, en Formosa, es el segundo humedal más grande de la Argentina y el tercero del Cono Sur. 400,000 Ha"
+    },
+
+]
