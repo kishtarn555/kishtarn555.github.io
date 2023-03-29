514058a54e1fff0888f17bf458ffd5f4ebcd0689
David Paredes <0238526@up.edu.mx>
2023-03-29 13:08:13 -0600

    Nigeria y Belize

---
+++ Nigeria/SobreElPais/info.js
@@ -0,0 +1,141 @@
+const historyData1 =  [
+    {
+        img: "indep.jpg", //la otra parte de la ruta esta en cards
+        title:"Independencia (GB)",
+        text:"1960"
+    },
+    {
+        img: "conflic.jpeg",
+        title:"Conflictos",
+        text:"Decenas de golpes de estado, guerras civiles, guerrillas de extremismo religioso."
+    },
+    {
+        img: "boko.jpeg",
+        title:"Boko Haram",
+        text:"Desde 2002, mujeres son arma de guerra. Promulgar Estado Islámico, matan cristianos."
+    },
+    {
+        img: "capital.jpeg",
+        title:"Capital",
+        text:"Abuya"
+    }
+
+]
+
+const historyData2 =  [
+    {
+        img: "niger.png", //la otra parte de la ruta esta en cards
+        title:"Procedencia",
+        text:"Nigeria -> Río Níger: el río más largo de África occidental. (4,200 km)"
+    },
+    {
+        img: "bandera.jpg",
+        title:"Bandera",
+        text:"Contradicción: riqueza natural y paz."
+    },
+    {
+        img: "moneda.jpg",
+        title:"Moneda oficial",
+        text:"Naira. (25 = 1 MXN)"
+    },
+    {
+        img: "idioma.png",
+        title:"Idioma oficial",
+        text:"Inglés"
+    },
+]
+
+ const poblacionData =  [
+    {
+        img: "pob1.jpg", //la otra parte de la ruta esta en cards
+        title:"País más poblado de áfrica, el 7mo en el mundo.",
+        text:"Tasa de natalidad de las más altas del planeta: 5.31 hijos por mujer vs 2.30 mundial."
+    },
+    {
+        img: "rural.jpeg",
+        title:"Zona Rural",
+        text:"53%"
+    }
+
+]
+
+const economiaData = [
+    {
+        img: "petro.jpeg", //la otra parte de la ruta esta en cards
+        title:"Dependiente del petróleo.",
+        text:"(80% de los ingresos presupuestarios). 10mo pais mundial en reservas."
+    },
+    {
+        img: "primario.jpeg",
+        title:"Sector primario",
+        text:"60% de la población"
+    },
+    {
+        img: "pobreza.jpeg",
+        title:"Pobreza extrema",
+        text:"50% subsisten con < de 1.9 USD al día"
+    }
+
+]
+
+const culturaData1 = [
+    {
+        img: "dialec.png", //la otra parte de la ruta esta en cards
+        title:"+500 dialectos. ",
+        text:"Hausa, Yoruba, Igbo"
+    },
+    {
+        img: "relig.jpg",
+        title:"Religión",
+        text:"Dos grandes grupos: musulmanes al norte, y al sur los cristianos."
+    },
+    {
+        img: "homo.jpg",
+        title:"Homosexuales",
+        text:"14 años de prision."
+    }
+
+]
+
+
+const culturaData2 = [
+    {
+        img: "nolly.jpg", //la otra parte de la ruta esta en cards
+        title:"Nollywood",
+        text:"Ttercera industria cinematográfica más rentable del mundo."
+    },
+    {
+        img: "corrup.jpg",
+        title:"Situación actual",
+        text:"Corrupción, violencia, problema de drogas, democracia poco transparente."
+    },
+    {
+        img: "defo.jpg",
+        title:"Deforestación",
+        text:"5% del territorio es forestal. Extinción de animales."
+    }
+
+]
+
+const interesData = [
+    {
+        img: "igo.jpeg", //la otra parte de la ruta esta en cards
+        title:"Igbo-Ora",
+        text:"Capital mundial de los mellizos. 58/1000 (vs USA 33/1000)."
+    },
+    {
+        img: "espac.jpeg",
+        title:"Programa espacial.",
+        text:""
+    },
+    {
+        img: "politic.jpeg",
+        title:"Falta de políticas",
+        text:"Consumo de carne de perro, mono y murciélago."
+    },
+    {
+        img: "osun.jpeg",
+        title:"Bosque Osun-Osogbo",
+        text:"Patrimonio de la Humanidad."
+    },
+]
