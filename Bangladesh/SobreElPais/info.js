f8554a40ddcf9ff8c7d2ae4b8dbe3021619fc236
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-03-29 04:32:22 -0600

    Titulos bangla

---
+++ Bangladesh/SobreElPais/info.js
@@ -0,0 +1,38 @@
+const historyData1 =  [
+    {
+        img: "sep_c1.png", //la otra parte de la ruta esta en cards
+        title:"Fundación",
+        text:"26 de marzo de 1971"
+    },
+    {
+        img: "",
+        title:"Colonia",
+        text:"Siglos XVI - XIX "
+    },
+    {
+        img: "bandera.png",
+        title:"Creación de bandera",
+        text:"1812"
+    },
+
+]
+
+const historyData2 =  [
+    
+    {
+        img: "independencia.jpg",
+        title:"Colonia Británica",
+        text:"India, Pakistán y Bangladés formaron parte de la Colonia India de Reino Unido"
+    },
+    {
+        img: "eastPakistan.jpg",
+        title:"Parte de Pakistán",
+        text:"1947 - India es separada en dos entidades por Inglaterra, una musulmana y otra hindú"
+    },
+    {
+        img: "founder.jpg",
+        title:"Independencia de Pakistán",
+        text:"26 de marzo de 1971"
+    },
+
+]
