1666ba57a90479f459f4294899473700508c8023
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-03-29 15:57:38 -0600

    Marruecos

---
+++ Morocco/SobreElPais/info.js
@@ -0,0 +1,80 @@
+const historyData1 =  [
+    {
+        img: "flag.png", //la otra parte de la ruta esta en cards
+        title:"Bandera",
+        text:"La bandera es roja, con una estrella verde al centro"
+    },
+    {
+        img: "Rabat.jpg",
+        title:"Rabat",
+        text:"Capital es Rabat, con una población de 577,827; 1,959,000 en la zona metropolitana. (Zona metropolitana de León tiene 2,139,484)"
+    },
+    {
+        img: "location.png",
+        title:"Ubicación",
+        text:"Se encuentra en el norte de África"
+    },
+
+]
+
+const historyData2 =  [
+    
+    {
+        img: "colony.png",
+        title:"Protectorado francés y Sahara español",
+        text:"El norte fue controlado por Francia y el sur por España, aunque fueron protectorados y no colonias"
+    },
+    {
+        img: "independency.webp",
+        title:"Independencia de Francia",
+        text:"1956 - Independencia de Francia, obtienen el territorio norte"
+    },
+    {
+        img: "morocco.webp",
+        title:"Independencia de España",
+        text:"1975 - Sahara occidental se independiza de españa, Marruecos reclama el territorio. Pero Sahara Occidental, o la República Árabe Saharaui Democrática se declará independiente de Marruecos"
+    }
+
+]
+
+const historyData3 =  [
+    
+    {
+        img: "other.svg",
+        title:"República Árabe Saharaui Democrática",
+        text:"También conodico como Sahara Occidental, país de reconocimiento limitado que disputa independencia con marruecos"
+    },
+    {
+        img: "territorio.png",
+        title:"Control defacto del territorio",
+        text:"Marruecos controla la mayoría del territorio de Sahara Occidental"
+    },
+    {
+        img: "recon.png",
+        title:"Reconocimiento",
+        text:"Países que reconocen a Sahara Occidental, México incluido"
+    },
+    {
+        img: "border.jpeg",
+        title:"Frontera",
+        text:"Cuenta con una zanja, metralletas y minas en la frontera entre el territorio controlado por Marruecos y Sahara Occidental"
+    }
+
+]
+
+
+
+const curiosityData =  [
+    
+    {
+        img: "verde.jpg",
+        title:"Agricultura",
+        text:"Representa el 14% del PIB y el 43% de la fuerza laboral"
+    },
+    {
+        img: "turism.jpg",
+        title:"Turismo",
+        text:"Representa la mitad de los ingresos de Marruecos"
+    }
+
+]
