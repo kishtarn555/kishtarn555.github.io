c1988a3c9a9c8cde921badf9a261e1e829385345
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-03-29 14:42:33 -0600

    coci

---
+++ Croatia/SobreElPais/info.js
@@ -0,0 +1,133 @@
+const historyData1 =  [
+    {
+        img: "flag.png", //la otra parte de la ruta esta en cards
+        title:"Bandera",
+        text:"La bandera es roja, blanca y azul, con un emblema central"
+    },
+    {
+        img: "zagreb.jpg",
+        title:"Zagreb",
+        text:"Capital es Zagreb, con una población de 779,145 (Aguascalientes tiene 948,990 sin contar la zona metropolitana)"
+    },
+    {
+        img: "location.png",
+        title:"Ubicación",
+        text:"Se encuentra en Europa en la península balcánica"
+    },
+
+]
+
+const historyData2 =  [
+    
+    {
+        img: "austria.png",
+        title:"Imperio Austro-Húngaro",
+        text:"Parte del imperio hasta la Primera Guerra Mundial"
+    },
+    {
+        img: "yugoslavia.png",
+        title:"Yugoslavia",
+        text:"1918 - Junto con actual Bosnia, Eslovenia, Macedonia, Montenegro, Serbia y Kosovo, formo parte de este reino"
+    },
+    {
+        img: "wave.jpg",
+        title:"República de Croacia",
+        text:"1991 - Croacia declara su independencia"
+    }
+
+]
+
+const historyData3 =  [
+    
+    {
+        img: "turismo.jpg",
+        title:"Turismo",
+        text:"Aproximadamente un quinto de los ingresos del país son del turismo"
+    },
+    {
+        img: "exportaciones.jpg",
+        title:"Exportaciones",
+        text:"Composición de las exportaciones, destacan los cruceros y el petroleo refinado"
+    },
+    {
+        img: "flame.png",
+        title:"Torres Flame",
+        text:"Son las torres Flame en Bakú, de los edificios mas reconocidos del País. Azerbaiyán se le conoce como 'La tierra de fuego'"
+    }
+
+]
+
+
+
+const curiosityData =  [
+    
+    {
+        img: "coci.jpg",
+        title:"COCI",
+        text:"Organizan el COCI (Concurso pre-universitario de programación), pero son de los pocos países pre-pandemia que realizaban sus concursos abiertos para todo el mundo. México utiliza estos concursos para entrenar, por eso elegí este país -Hector"
+    },
+    {
+        img: "dance.jpg",
+        title:"Patrimonio Intangible de la Humanidad",
+        text:"Es uno de los 9 países que tienen más de 15, (tiene 18, México tiene 11)"
+    },
+    {
+        img: "catedral.jpg",
+        title:"Catedral de Zagreb",
+        text:""
+    },
+    {
+        img: "csto.png",
+        title:"Armenia es parte de CSTO",
+        text:"Armenia es aliada con CSTO (Rusia). Azerbaiyán es aliada con Turquía (Turkiye)"
+    },
+
+]
+
+
+const historyData4 =  [
+    {
+        img: "fire.jpg",
+        title:"IOI 2019",
+        text:"Anfitrión del IOI 2019, por eso estuve allí."
+    },
+    {
+        img: "olympics.jpg",
+        title:"Olimpiadas",
+        text:"Anfitriones de los primeros juegos olímpicos europeos en 2015 (Villa olímpica)"
+    },
+    
+    {
+        img: "auditorio.jpg",
+        title:"Heydar Aliyev Center",
+        text:"Construido por la famosa arquitecta Zaha Hadid"
+    },
+
+]
+
+
+const historyData5 =  [
+    {
+        img: "russia.png",
+        title:"Ruso",
+        text:"El idioma ruso todavía se encuentra muy presente en el país, 38% lo hablan"
+    },
+    {
+        img: "firewall.jpg",
+        title:"Yanar Dag",
+        text:"Flama de gas natural, haciendo honor a la 'tierra del fuego'"
+    },
+    
+    {
+        img: "doncella.jpg",
+        title:"Torre de la doncella",
+        text:"Construcción famosa e histórica del sigo XII"
+    },
+    {
+        img: "hotel.jpg",
+        title:"Vista de nuestro Hotel",
+        text:""
+    },
+
+]
+
