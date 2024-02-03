512f73602396a653026c9f1c7fb77089d9ca10f6
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2024-02-03 10:07:11 -0600

    added ReKarel

---
+++ ReKarel/webapp/ts/KarelController.ts
@@ -0,0 +1,252 @@
+import { World, compile, detectLanguage } from "../../js/karel";
+import { WorldController } from "./worldController";
+import { EditorView } from "codemirror";
+import { EditorState, StateEffect } from "@codemirror/state"
+import { DesktopController } from "./desktop-ui";
+import { ERRORCODES } from "./common-ui";
+
+type messageType = "info"|"success"|"error";
+type MessageCallback = (message:string, type:messageType)=>void;
+type ControllerState = "unstarted"| "running" | "finished";
+type StateChangeCallback = (caller:KarelController, newState:ControllerState)=>void;
+type StepCallback = (caller:KarelController, newState:ControllerState)=>void;
+class KarelController {
+    world: World;
+    desktopController: WorldController;
+    running: boolean;
+    mainEditor: EditorView;
+    private onMessage: MessageCallback[];
+    private onStateChange: StateChangeCallback[];
+    private onStep: StepCallback[];
+    private state : ControllerState;
+    private endedOnError:boolean;
+    private autoStepInterval:number;
+    private drawFrameRequest : number;
+    private autoStepping: boolean;
+
+    constructor(world: World, mainEditor: EditorView) {
+        this.world = world;
+        this.running = false;
+        this.mainEditor = mainEditor;
+        this.onMessage = [];
+        this.onStateChange = [];
+        this.onStep = [];
+        this.state = "unstarted";
+        this.endedOnError = false;
+        this.autoStepInterval = 0;
+        this.autoStepping = false;
+    }
+
+    SetWorld (world: World) {
+        this.world = world;
+        this.SetDesktopController(this.desktopController);
+    }
+    
+    SetDesktopController(desktopController: WorldController) {
+        this.desktopController = desktopController;
+        this.desktopController.SetWorld(this.world);
+    }
+
+    Compile() {
+        let code = this.mainEditor.state.doc.toString();
+        // let language: string = detectLanguage(code);
+        let response = null;
+        try {
+            response = compile(code);
+            //TODO: expand message            
+            this.SendMessage("Programa compilado correctamente", "info");
+        } catch (e) {
+            //TODO: Expand error
+            this.SendMessage("Error de compilacion", "error");
+        }
+        
+        return response;
+    }
+
+    // FIXME This is code from karel.js that I'm not even sure if it's ever executed by the web app.
+    validatorCallbacks(message) {
+        console.log("validator said this: ", message);
+    }
+    
+    IsAutoStepping(): boolean {
+        return this.autoStepping;
+    }
+
+    GetState(): ControllerState {
+        return this.state;
+    }
+
+    Reset() {        
+        this.endedOnError = false;
+        this.running = false;
+        this.ChangeState("unstarted");
+        this.desktopController.Reset();
+    }
+
+    StartRun(): boolean {        
+        this.endedOnError = false;
+        let compiled = this.Compile();
+        if (compiled == null) {
+            return false;
+        }
+        this.Reset();
+        let runtime = this.desktopController.GetRuntime();        
+        runtime.load(compiled);
+        // FIXME: We skip validators, they seem useless, but I'm unsure
+        
+        runtime.start();
+        this.running = true;
+        this.ChangeState("running");
+        return true;        
+    }
+
+    HighlightCurrentLine() {
+        let runtime= this.desktopController.GetRuntime();
+        if (runtime.state.line >= 0) {          
+            
+            let codeLine = this
+                .mainEditor
+                .state
+                .doc
+                .line(
+                    runtime.state.line+1
+                );
+            this.mainEditor.dispatch({
+                selection:{
+                    anchor: codeLine.from,
+                    head: codeLine.from
+                }                
+            });
+        }
+      }
+
+    Step() {
+        if (this.state === "finished") {
+            //Ignore if the code already finished running
+            return;
+        }
+        if (!this.running) {
+            if (!this.StartRun()) {
+                // Code Failed
+                return;
+            }
+        }
+        
+        let runtime = this.desktopController.GetRuntime();
+        runtime.step();
+        this.HighlightCurrentLine();
+        this.desktopController.CheckUpdate();
+
+        if (!runtime.state.running) {            
+            this.EndMessage();
+            this.ChangeState("finished");
+        }
+
+        this.NotifyStep();
+
+    }
+
+    StartAutoStep(delay:number) {        
+        this.StopAutoStep(); //Avoid thread leak
+        if (this.state === "finished") {
+            return;
+        }
+        this.autoStepping = true;
+        if (!this.running) {
+            if (!this.StartRun()) {
+                //Code Failed
+                return;
+            }
+        }
+        this.autoStepInterval = setInterval(
+            ()=>{
+                if (!this.running) {
+                    this.StopAutoStep();
+                    return;
+                }
+                this.Step();
+                this.desktopController.CheckUpdate();
+            }, 
+            delay
+        );
+    }
+
+    ChangeAutoStepDelay(delay:number) {
+        if (!this.IsAutoStepping()) {
+            return;
+        }
+        this.StartAutoStep(delay);
+    }
+
+    StopAutoStep() {
+        this.autoStepping = false;
+        if (this.autoStepInterval !== 0) {
+            clearInterval(this.autoStepInterval);
+            this.autoStepInterval = 0;
+        }
+    }
+
+
+    EndedOnError() {
+        return this.endedOnError;
+    }
+    RunTillEnd() {
+        if (this.state === "finished") {
+            return;
+        }
+        if (!this.running) {
+            if (!this.StartRun()) {
+                return;
+            }
+        }
+
+        let runtime = this.desktopController.GetRuntime();
+        runtime.disableStackEvents= true; // FIXME: This should only be done when no breakpoints
+        while (runtime.step());
+        runtime.disableStackEvents= false; // FIXME: This should only be done when no breakpoints
+        this.desktopController.CheckUpdate();
+        this.EndMessage();
+        this.ChangeState("finished");
+    }
+
+    RegisterMessageCallback(callback: MessageCallback) {
+        this.onMessage.push(callback);
+    }
+
+    RegisterStateChangeObserver(callback: StateChangeCallback) {
+        this.onStateChange.push(callback);
+    }
+
+    RegisterStepController(callback: StepCallback) {
+        this.onStep.push(callback);
+    }
+
+    private SendMessage(message: string, type: messageType) {
+        this.onMessage.forEach((callback) => callback(message, type));
+    }
+
+    private NotifyStateChange() {
+        this.onStateChange.forEach((callback) => callback(this, this.state));
+    }
+    private NotifyStep() {
+        this.onStep.forEach((callback) => callback(this, this.state));
+    }
+
+    
+    private ChangeState(nextState: ControllerState) {
+        this.state = nextState;
+        this.NotifyStateChange();
+    }
+
+    private EndMessage() {
+        let runtime = this.desktopController.GetRuntime();
+        if (runtime.state.error) {
+            this.SendMessage(ERRORCODES[runtime.state.error], "error");            
+            this.endedOnError = true;
+            return;
+        }
+        this.SendMessage("Ejecucion terminada exitosamente!", "success");
+    }
+}
+
+export {KarelController, ControllerState};
---
+++ ReKarel/webapp/ts/worldController.ts
@@ -0,0 +1,382 @@
+import { WorldRenderer } from "./worldRenderer";
+import { World } from "../../js/karel";
+import { karel } from "../../js";
+import { redoDepth } from "@codemirror/history";
+import { Collapse } from "bootstrap";
+
+type CellSelection = {
+    r: number,
+    c: number,
+    rows: number,
+    cols: number,
+    dr: number,
+    dc: number,
+}
+
+type Gizmos = {
+    selectionBox: {
+        main: HTMLElement,
+        left: HTMLElement,
+        right: HTMLElement,
+        top: HTMLElement,
+        bottom: HTMLElement,
+    }
+}
+
+class WorldController {
+    renderer: WorldRenderer
+    container: HTMLElement
+    gizmos: Gizmos
+    world: World;
+    scale: number;
+    state: {
+        cursorX: number,
+        cursorY: number,
+    }
+    selection: CellSelection;
+    private lock: boolean;
+
+
+    constructor(renderer: WorldRenderer, container: HTMLElement, world: World, gizmos: Gizmos) {
+        this.renderer = renderer;
+        this.container = container;
+        this.world = world;
+        this.lock = false;
+        this.selection = {
+            r: 1,
+            c: 1,
+            rows: 1,
+            cols: 1,
+            dr: 1,
+            dc: 1,
+        };
+        this.state = {
+            cursorX: 0,
+            cursorY: 0,
+        }
+        this.gizmos = gizmos;
+        this.scale = 1;
+    }
+
+    Lock() {
+        this.lock = true;
+    }
+
+    UnLock() {
+        this.lock = false;
+    }
+
+    Reset() {
+        this.world.reset();
+        this.Update();
+    }
+
+    GetRuntime() {
+        return this.world.runtime;
+    }
+
+    
+    GetBeepersInBag(): number {
+        return this.world.bagBuzzers;
+    }
+
+    SetBeepersInBag(ammount : number) {
+        this.world.setBagBuzzers(ammount);
+    }
+
+    SetWorld(world: World) {
+        this.world = world;
+        this.Update();
+        this.Select(1,1,1,1);    
+    }
+
+    CheckUpdate() {
+        if (this.world.dirty) {
+            this.Update();
+        }
+    }
+
+    ErrorMode() {
+        this.renderer.ErrorMode();
+        this.Update();
+    }
+
+    NormalMode() {
+        this.renderer.NormalMode();
+        this.Update();
+    }
+
+    Select(r: number, c: number, r2: number, c2: number) {
+        this.selection = {
+            r: r,
+            c: c,
+            rows: Math.abs(r - r2) + 1,
+            cols: Math.abs(c - c2) + 1,
+            dr: r <= r2 ? 1 : -1,
+            dc: c <= c2 ? 1 : -1,
+        };
+        this.UpdateWaffle();
+    }
+
+    MoveSelection(dr: number, dc: number) {
+        let r = this.selection.r+dr;
+        let c =this.selection.c+dc;
+
+        if (r < 1 || c < 1 || r > this.world.h || c > this.world.w) {
+                return;
+        }
+        this.Select(
+            this.selection.r+dr,
+            this.selection.c+dc,
+            this.selection.r+dr,
+            this.selection.c+dc,
+        );
+    }
+
+    UpdateWaffle() {
+        let coords = this.renderer.CellToPoint(this.selection.r, this.selection.c);
+        let selectionBox = this.gizmos.selectionBox.main;
+        selectionBox.style.top = `${coords.y / window.devicePixelRatio}px`
+        selectionBox.style.left = `${coords.x / window.devicePixelRatio}px`
+    }
+
+    SetScale(scale: number) {
+        this.renderer.scale = scale * window.devicePixelRatio;
+        this.scale = scale;
+        //FIXME, this should be in update waffle
+        this.gizmos.selectionBox.bottom.style.maxWidth = `${this.renderer.CellSize * scale}px`;
+        this.gizmos.selectionBox.bottom.style.minWidth = `${this.renderer.CellSize * scale}px`;
+        
+        this.gizmos.selectionBox.top.style.maxWidth = `${this.renderer.CellSize * scale}px`;
+        this.gizmos.selectionBox.top.style.minWidth = `${this.renderer.CellSize * scale}px`;
+        
+        this.gizmos.selectionBox.left.style.maxHeight = `${this.renderer.CellSize * scale}px`;
+        this.gizmos.selectionBox.left.style.minHeight = `${this.renderer.CellSize * scale}px`;
+        
+        this.gizmos.selectionBox.right.style.maxHeight = `${this.renderer.CellSize * scale}px`;
+        this.gizmos.selectionBox.right.style.minHeight = `${this.renderer.CellSize * scale}px`;
+
+        this.gizmos.selectionBox.bottom.style.top = `${this.renderer.CellSize * scale}px`;
+        this.gizmos.selectionBox.right.style.left = `${this.renderer.CellSize * scale}px`;
+
+
+        this.UpdateWaffle();
+        this.Update();        
+    }
+
+    TrackMouse(e: MouseEvent) {
+        let canvas = this.renderer.canvasContext.canvas;
+        let boundingBox = canvas.getBoundingClientRect();
+        let x = (e.clientX - boundingBox.left) * canvas.width / boundingBox.width;
+        let y = (e.clientY - boundingBox.top) * canvas.height / boundingBox.height;
+        this.state.cursorX = x / this.renderer.scale;
+        this.state.cursorY = y / this.renderer.scale;
+    }
+
+    ClickUp(e: MouseEvent) {
+        let cell = this.renderer.PointToCell(this.state.cursorX, this.state.cursorY);
+        if (cell.r < 0) {
+            return;
+        }
+        this.Select(cell.r, cell.c, cell.r, cell.c);
+        console.log(this.selection.r, this.selection.c);
+
+
+    }
+
+    SetKarelOnSelection(direction: "north" | "east" | "west" | "south" = "north") {
+        if (this.lock) return;
+        this.world.move(this.selection.r, this.selection.c);
+        switch (direction) {
+            case "north":
+                this.world.rotate('NORTE');
+                break;
+            case "east":
+                this.world.rotate('ESTE');
+                break;
+            case "south":
+                this.world.rotate('SUR');
+                break;
+            case "west":
+                this.world.rotate('OESTE');
+                break;
+        }
+        this.Update();
+
+    }
+
+    ChangeBeepers(delta: number) {
+        if (this.lock) return;
+        if (delta === 0) {
+            return;
+        }
+        let buzzers = this.world.buzzers(this.selection.r, this.selection.c);
+        if (buzzers < 0 && delta < 0) {
+            //Do nothing
+            return;
+        }
+        buzzers += delta;
+        if (buzzers < 0) {
+            buzzers = 0;
+        }
+        this.world.setBuzzers(
+            this.selection.r,
+            this.selection.c,
+            buzzers
+        );
+        this.Update();
+    }
+
+    SetBeepers(ammount: number) {
+        if (this.lock) return;
+        if (this.world.buzzers(this.selection.r, this.selection.c) === ammount) {
+            return;
+        }
+        this.world.setBuzzers(this.selection.r, this.selection.c, ammount);
+        this.Update();
+    }
+
+    ToggleKarelPosition() {
+        if (this.lock) return;
+        this.world.move(this.selection.r, this.selection.c);
+        this.world.rotate();
+        this.Update();
+    }
+
+    FocusOrigin() {
+        this.container.scrollLeft = 0;
+        this.container.scrollTop = this.container.scrollHeight - this.container.clientHeight;
+    }
+
+    FocusKarel() {
+        let r = this.world.i - 2;
+        let c = this.world.j - 2;
+
+        this.FocusTo(r, c);
+    }
+    
+    FocusSelection() {
+        let r = this.selection.r - 2;
+        let c = this.selection.c - 2;
+
+        this.FocusTo(r, c);
+    }
+
+    FocusTo(r: number, c: number) {
+
+
+        let left = c / (this.world.w - this.renderer.GetColCount("floor") + 1);
+        left = left < 0 ? 0 : left;
+        left = left > 1 ? 1 : left;
+        let top = r / (this.world.h - this.renderer.GetRowCount("floor") + 1);
+        top = top < 0 ? 0 : top;
+        top = top > 1 ? 1 : top;
+        console.log(top);
+        this.container.scrollLeft = left * (this.container.scrollWidth - this.container.clientWidth);
+        this.container.scrollTop = (1 - top) * (this.container.scrollHeight - this.container.clientHeight);
+    }
+
+    ToggleWall(which: "north" | "east" | "west" | "south" | "outer") {
+        if (this.lock) return;
+        switch (which) {
+            case "north":
+                for (let i = 0; i < this.selection.rows; i++) {
+                    let r = this.selection.r + i * this.selection.dr;
+                    let c = Math.min(
+                        this.selection.c,
+                        this.selection.c + (this.selection.cols - 1) * this.selection.dc
+                    );
+                    this.world.toggleWall(r, c, 1);
+                }
+                break;
+            case "south":
+                for (let i = 0; i < this.selection.rows; i++) {
+                    let r = this.selection.r + i * this.selection.dr;
+                    let c = Math.max(
+                        this.selection.c,
+                        this.selection.c + (this.selection.cols - 1) * this.selection.dc
+                    );
+                    this.world.toggleWall(r, c, 3);
+                }
+                break;
+            case "west":
+                for (let i = 0; i < this.selection.cols; i++) {
+                    let r = Math.min(
+                        this.selection.r,
+                        this.selection.r + (this.selection.rows - 1) * this.selection.dr
+                    );
+                    let c = this.selection.c + i * this.selection.dc;
+                    this.world.toggleWall(r, c, 0);
+                }
+                break;
+            case "east":
+                for (let i = 0; i < this.selection.cols; i++) {
+                    let r = Math.max(
+                        this.selection.r,
+                        this.selection.r + (this.selection.rows - 1) * this.selection.dr
+                    );
+                    let c = this.selection.c + i * this.selection.dc;
+                    this.world.toggleWall(r, c, 2);
+                }
+                break;
+            case "outer":
+                for (let i = 0; i < this.selection.cols; i++) {
+                    let rmin = Math.min(
+                        this.selection.r,
+                        this.selection.r + (this.selection.rows - 1) * this.selection.dr
+                    );
+                    let rmax = Math.max(
+                        this.selection.r,
+                        this.selection.r + (this.selection.rows - 1) * this.selection.dr
+                    );
+                    let c = this.selection.c + i * this.selection.dc;
+                    this.world.toggleWall(rmin, c, 2);
+                    this.world.toggleWall(rmax, c, 0);
+                }
+                for (let i = 0; i < this.selection.rows; i++) {
+                    let r = this.selection.r + i * this.selection.dr;
+                    let cmin = Math.min(
+                        this.selection.c,
+                        this.selection.c + (this.selection.cols - 1) * this.selection.dc
+                    );
+                    let cmax = Math.max(
+                        this.selection.c,
+                        this.selection.c + (this.selection.cols - 1) * this.selection.dc
+                    );
+                    this.world.toggleWall(r, cmin, 1);
+                    this.world.toggleWall(r, cmax, 3);
+                }
+                break;
+        }
+        this.Update();
+    }
+
+    UpdateScroll(left: number, top: number): void {
+        let worldWidth = this.world.w;
+        let worldHeight = this.world.h;
+
+        this.renderer.origin = {
+            f: Math.floor(
+                1 + Math.max(
+                    0,
+                    (worldHeight - this.renderer.GetRowCount("floor") + 1) * top
+                )
+            ),
+            c: Math.floor(
+                1 + Math.max(
+                    0,
+                    (worldWidth - this.renderer.GetColCount("floor") + 1) * left
+                )
+            ),
+        }
+        this.Update();
+        this.UpdateWaffle();
+    }
+
+    Update() {
+        this.world.dirty=false;
+        this.renderer.Draw(this.world);
+    }
+}
+
+export { WorldController, Gizmos };
