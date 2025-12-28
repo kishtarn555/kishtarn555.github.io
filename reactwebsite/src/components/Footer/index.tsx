fde601b96fdd4db1f8b918a9662f25fe321f3078
kishtarn555 <hec_montemoon@hotmail.com>
2025-12-28 00:09:54 -0600

    [WEB] Better Footer

--- reactwebsite/src/components/Footer/index.tsx
+++ reactwebsite/src/components/Footer/index.tsx
@@ -1,19 +1,16 @@
-import { useLanguage } from "../../lang/languageContext";
-
-const languages = {
-  en: "English",
-  es: "Español",
-  ja: "日本語",
-}
+import React from "react";
 
 const MainNavbar: React.FC = () => {
-  const { t, setLanguage, language } = useLanguage();
 
   return (
     <>
       <hr/>
-      <div className="d-flex justify-content-center align-items-center p-3">
-        <span className="me-3">&copy; 2025 Hector Ricardez. All rights reserved.</span>
+      <div className="d-flex justify-content-center align-items-center ps-3 pe-3">
+        <span className="me-2 ms-2">&copy; 2025 Hector Ricardez. All rights reserved.</span>
+      </div>
+      <div className="d-flex justify-content-center align-items-center ps-3 pe-3 mb-3 ">        
+        <a href="https://www.instagram.com/hec_kishtarn/" className="me-2 ms-2"><i className="bi bi-instagram"></i> hec_kishtarn </a>
+        <a href="https://github.com/kishtarn555" className="me-2 ms-2"><i className="bi bi-github"></i> kishtarn555 </a>
       </div>
     </>
   );
