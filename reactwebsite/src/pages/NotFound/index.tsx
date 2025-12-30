d0dd15c43d66c0fe78f5fd0382aaeba543fa2f89
kishtarn555 <hec_montemoon@hotmail.com>
2025-12-29 23:50:59 -0600

    [WEB] Better 404

--- reactwebsite/src/pages/NotFound/index.tsx
+++ reactwebsite/src/pages/NotFound/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import ConstructionWarning from '../../components/warnings/construction';
 
 const NotFound: React.FC = () => {
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>404 - Page Not Found</h1>
             <p>The page you are looking for does not exist.</p>
+            <ConstructionWarning/>
+            <a href='/'>
+                <i className="bi bi-house-fill"></i>{" "}
+                Go back to home
+
+            </a>
         </div>
     );
 }
